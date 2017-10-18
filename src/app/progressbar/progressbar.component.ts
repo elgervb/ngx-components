07c096b14986bba802a2dce1da9313fb281b900b
Elger van Boxtel <elger.van.boxtel@isatis-health.nl>
2017-10-18 22:13:11 +0200

    Progressbar: customize with inputs

--- src/app/progressbar/progressbar.component.ts
+++ src/app/progressbar/progressbar.component.ts
@@ -3,15 +3,21 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 @Component({
   selector: 'evb-progressbar',
   template: `
-  <div class="progressbar">
-    <div class="progressbar__inner"[style.width.%]="progress"></div>
-  </div>
+    <div class="progressbar"
+      [class.progress--with-text]="showText"
+      [class.progress--gt-50]="progress >= 50"
+      [style.height.px]="height"
+      [attr.data-progress]="progress">
+      <div class="progressbar__inner" [style.width.%]="progress"></div>
+    </div>
   `,
   styleUrls: ['./progressbar.component.scss']
 })
 export class ProgressbarComponent implements OnChanges {
 
   @Input() progress = 0;
+  @Input() showText = false;
+  @Input() height?;
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['progress']) {
