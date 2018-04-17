4cd3c1e4f4e78c9203ba0eb5ec8d5e4c87d4c71a
Elger van Boxtel <elger.van.boxtel@isatis-health.nl>
2018-04-17 16:40:32 +0200

    button: add some extra tests

--- src/app/button/button.component.spec.ts
+++ src/app/button/button.component.spec.ts
@@ -8,9 +8,9 @@ describe('ButtonComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
+      declarations: [ButtonComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +21,38 @@ describe('ButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be able to set the type', () => {
+    component.type = 'mock-button';
+    fixture.detectChanges();
+
+    const el = fixture.debugElement.nativeElement;
+
+    const type = el.querySelector('button').getAttribute('type');
+    expect(type).toBe('mock-button');
+  });
+
+  it('should be possible to set css class', () => {
+    component.class = 'btn-mock';
+    fixture.detectChanges();
+
+    const el = fixture.debugElement.nativeElement;
+
+    const button: HTMLButtonElement = el.querySelector('button');
+    expect(button).toBeDefined();
+    expect(button.classList.contains('btn-mock')).toBe(true);
+  });
+
+  it('should be possible to set multiple css classes', () => {
+    component.class = 'btn-mock btn-mock-2';
+    fixture.detectChanges();
+
+    const el = fixture.debugElement.nativeElement;
+
+    const button: HTMLButtonElement = el.querySelector('button');
+    expect(button).toBeDefined();
+    expect(button.classList.contains('btn-mock')).toBe(true);
+    expect(button.classList.contains('btn-mock-2')).toBe(true);
   });
 });
