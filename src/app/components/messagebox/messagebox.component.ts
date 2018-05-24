ebb6c93e9c003087e9c9c77a3a7d55b9d598ea26
Elger van Boxtel <elger.van.boxtel@isatis-health.nl>
2018-05-24 13:49:12 +0200

    messagebox

---
+++ src/app/components/messagebox/messagebox.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'evb-messagebox',
+  template: `<ng-content></ng-content>`,
+  styleUrls: ['./messagebox.component.scss']
+})
+export class MessageboxComponent {
+  @Input() class = '';
+  @Input() type: 'info' | 'error';
+
+  @HostBinding('class')
+  get hostClasses(): string {
+    return `${this.class} messagebox messagebox--${this.type}`;
+  }
+}
