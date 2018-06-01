58cf399c8f5039af6643f933028628b54d74342e
Elger van Boxtel <elgervb@gmail.com>
2018-06-01 22:48:43 +0200

    container: new container components

---
+++ src/app/components/container/container/container.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'evb-container',
+  template: `<ng-content></ng-content>`,
+  styleUrls: ['./container.component.scss']
+})
+export class ContainerComponent implements OnInit {
+
+  @Input() class = '';
+  @Input() dir?: 'row' | 'col';
+  @Input() justify: 'left' | 'right' | 'center' | 'around' | 'evenly' | 'between';
+  @Input() root: boolean;
+
+  @HostBinding('class')
+  get hostClasses(): string {
+    const dirClass = this.dir ? `evb-container--${this.dir}` : '';
+    const rootClass = this.root ? 'evb-container--root' : '';
+    const justifyClass = this.justify ? `evb-container--${this.justify}` : '';
+    return `${this.class} evb-container ${dirClass} ${rootClass} ${justifyClass}`.trim();
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
