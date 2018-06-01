ba2f59c9322f3e35f494ae145df34b8994424f87
Elger van Boxtel <elgervb@gmail.com>
2018-06-02 00:16:17 +0200

    card: done

--- src/app/components/card/card-body/card-body.component.ts
+++ src/app/components/card/card-body/card-body.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'evb-card-body',
   template: `<ng-content></ng-content>`,
-  styleUrls: ['./card-body.component.scss']
+  styleUrls: ['./card-body.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CardBodyComponent implements OnInit {
 
