5fe8a6d1c4684f5b197aecf9f3c86cacd65ce233
Elger van Boxtel <elger.van.boxtel@isatis-health.nl>
2018-06-20 12:27:46 +0200

    flyout: add animation, pass result when close and close on esc key

--- src/app/components/flyout/flyout/flyout.component.ts
+++ src/app/components/flyout/flyout/flyout.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit, AfterContentInit, ContentChild } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ContentChild, HostListener } from '@angular/core';
 import { FlyoutHeaderComponent } from '../flyout-header/flyout-header.component';
+import { trigger, transition, style, animate, state } from '@angular/animations';
+import { Observable, Subject } from 'rxjs';
+
+const animationDuration = 250;
 
 @Component({
   selector: 'evb-flyout',
   template: `
-    <aside class="flyout" *ngIf="isOpen">
+    <aside class="flyout" *ngIf="isOpen"  [@flyInOut]="isOpen">
       <ng-content></ng-content>
     </aside>
   `,
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [ // :enter
+        style({ transform: 'translateX(-100%)' }),
+        animate(animationDuration)
+      ]),
+      transition('* => void', [ // :leave
+        animate(animationDuration, style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ],
   styleUrls: ['./flyout.component.scss']
 })
 export class FlyoutComponent implements OnInit, AfterContentInit {
@@ -18,7 +34,15 @@ export class FlyoutComponent implements OnInit, AfterContentInit {
     return this._open;
   }
 
+  // tslint:disable-next-line no-any the result can return anyting
+  private result = new Subject<any>();
+
   private _open = false;
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onKeydownHandler() {
+    this.close();
+  }
 
   constructor() { }
 
@@ -32,16 +56,33 @@ export class FlyoutComponent implements OnInit, AfterContentInit {
     }
   }
 
-  open() {
+  /**
+   * Open the flyout and get a result `Observable`
+   * No need to unsubscribe from this `Observable` as it will be automatically completed when the
+   * flyout closes
+   */
+  // tslint:disable-next-line no-any the result can return anyting
+  open(): Observable<any> {
     this._open = true;
+    return this.result.asObservable();
   }
 
-  close() {
+  /**
+   * Close the flyout and optionally pass a result
+   */
+  // tslint:disable-next-line no-any the result can return anyting
+  close(result?: any) {
     this._open = false;
+    this.result.next(result);
+    this.result.complete();
   }
 
-  toggle() {
-    this._open = !this._open;
+  /**
+   * Toggle the flyout and optionally pass a result (when closing)
+   */
+  // tslint:disable-next-line no-any the result can return anyting
+  toggle(result?: any) {
+    this._open ? this.close(result) : this.open();
   }
 
 }
